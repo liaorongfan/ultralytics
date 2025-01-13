84166a25d89edfe56223a6820f37e1d78e26c538
liaorongfan <18616147936@163.com>
2025-01-13 09:02:04 +0800

    remove p3 layer

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Load a model
-    model = YOLO("yolo11s.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo11l.yaml").load("yolo11l.pt")  # load a pretrained model (recommended for training)
 
     # Train the model
     results = model.train(data="VOC.yaml", epochs=100, imgsz=640)
